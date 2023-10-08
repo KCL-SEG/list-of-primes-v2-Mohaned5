40f335d71cbb112e9e9e76d2fa0986dd426faf7b
mohaned5 <mohaned@live.co.uk>
2023-10-08 01:08:29 +0100

    completed task

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    try:
+        list = []
+        if number_of_primes <= 0:
+            raise ValueError
+        num = 2
+        while len(list) < number_of_primes:
+            is_prime = True
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                list.append(num)
+            num += 1
+        return list
+    except ValueError as e:
+        raise e
+        
